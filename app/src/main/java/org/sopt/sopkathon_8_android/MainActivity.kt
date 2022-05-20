2edea29564f5efbb5cb93d4138068348fc15a179
hansh0101 <hansh0101@naver.com>
2022-05-20 23:09:48 +0900

    Initial commit

---
+++ app/src/main/java/org/sopt/sopkathon_8_android/MainActivity.kt
@@ -0,0 +1,14 @@
+package org.sopt.sopkathon_8_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("승현", "하이")
+    }
+}
