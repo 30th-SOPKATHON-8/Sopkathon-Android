38e814996eb9350259db9bcf0bec275c7a9ede9f
yoon <cbj0010@naver.com>
2022-05-22 04:24:11 +0900

    [FEAT] #8 - DataBinding 구현

--- app/src/main/java/org/sopt/sopkathon_8_android/presentation/write/ReceiptFragment.kt
+++ app/src/main/java/org/sopt/sopkathon_8_android/presentation/write/ReceiptFragment.kt
@@ -4,19 +4,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import org.sopt.sopkathon_8_android.R
 import org.sopt.sopkathon_8_android.databinding.FragmentReceiptBinding
+import org.sopt.sopkathon_8_android.presentation.viewmodel.WriteViewModel
 
 
 class ReceiptFragment : Fragment() {
     private var _binding: FragmentReceiptBinding? = null
     private val binding: FragmentReceiptBinding get() = _binding ?: error("binding이 연결안되었슴다.")
+    private val viewModel by activityViewModels<WriteViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentReceiptBinding.inflate(layoutInflater, container, false)
+        _binding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_receipt, container, false)
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
         return binding.root
     }
 
