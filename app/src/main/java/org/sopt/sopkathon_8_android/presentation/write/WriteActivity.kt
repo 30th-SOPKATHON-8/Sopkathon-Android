9e5ba567ad0229cf13fd692abd315ef6c6402fc7
hansh0101 <hansh0101@naver.com>
2022-05-22 03:28:44 +0900

    [FEAT] #4 - Implement WriteFragment

--- app/src/main/java/org/sopt/sopkathon_8_android/presentation/write/WriteActivity.kt
+++ app/src/main/java/org/sopt/sopkathon_8_android/presentation/write/WriteActivity.kt
@@ -2,11 +2,25 @@ package org.sopt.sopkathon_8_android.presentation.write
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import org.sopt.sopkathon_8_android.R
+import org.sopt.sopkathon_8_android.databinding.ActivityWriteBinding
 
 class WriteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWriteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_write)
+        binding = ActivityWriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initFragmentContainerView()
+    }
+
+    private fun initFragmentContainerView() {
+        supportFragmentManager.commit {
+            add<WriteFragment>(R.id.fcv_write, "WriteFragment")
+        }
     }
 }
