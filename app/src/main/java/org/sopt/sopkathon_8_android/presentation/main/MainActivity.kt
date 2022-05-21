1e198014be002fde8f0c989344ae62d306c05b34
urjimyu <aliyah_7@naver.com>
2022-05-22 03:49:42 +0900

    [FEAT] #2 - UI 마무리 구현

--- app/src/main/java/org/sopt/sopkathon_8_android/presentation/main/MainActivity.kt
+++ app/src/main/java/org/sopt/sopkathon_8_android/presentation/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package org.sopt.sopkathon_8_android.presentation.main
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import org.sopt.sopkathon_8_android.databinding.ActivityMainBinding
 
@@ -11,5 +12,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        clickAll()
+        clickGood()
+        clickBad()
+    }
+
+    private fun clickAll() {
+        binding.tvTotal.setOnClickListener {
+            binding.vTabLine1.visibility = View.VISIBLE
+            binding.vTabLine2.visibility = View.INVISIBLE
+            binding.vTabLine3.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun clickGood() {
+        binding.tvGood.setOnClickListener {
+            binding.vTabLine2.visibility = View.VISIBLE
+            binding.vTabLine1.visibility = View.INVISIBLE
+            binding.vTabLine3.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun clickBad() {
+        binding.tvBad.setOnClickListener {
+            binding.vTabLine3.visibility = View.VISIBLE
+            binding.vTabLine2.visibility = View.INVISIBLE
+            binding.vTabLine1.visibility = View.INVISIBLE
+        }
     }
 }
