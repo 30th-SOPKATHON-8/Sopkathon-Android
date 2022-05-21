e0afd914d028f2370f7ec47fe894a9376ba155ad
urjimyu <aliyah_7@naver.com>
2022-05-22 01:48:50 +0900

    [FEAT] #2 - ItemData xml 작성

--- app/src/main/java/org/sopt/sopkathon_8_android/presentation/main/ItemData.kt
+++ app/src/main/java/org/sopt/sopkathon_8_android/presentation/main/ItemData.kt
@@ -1,3 +1,10 @@
 package org.sopt.sopkathon_8_android.presentation.main
 
-data class ItemData()
+data class ItemData(
+    val _id: String,
+    val title: String,
+    val content: String,
+    val price: Int,
+    val createdAt: String,
+    val isXibal: Boolean
+)
